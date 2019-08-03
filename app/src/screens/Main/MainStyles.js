5aaba558807452b4b973e1e3a3fdb6bd87eeccb5
shadman96 <mdsadman96@gmail.com>
2019-08-04 04:48:44 +0530

    AssignMent Completed.

---
+++ app/src/screens/Main/MainStyles.js
@@ -0,0 +1,9 @@
+export default style = {
+col1:
+    { backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' },
+    col2:{ backgroundColor: 'green', justifyContent: 'center', alignItems: 'center' },
+    col3:{ backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center' },
+    rowContent:{ backgroundColor: 'red', marginTop: 16 },
+    buttonStyle:{ justifyContent: 'center', margin: 10 },
+    
+ }
